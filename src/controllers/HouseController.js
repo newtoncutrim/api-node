19cbf6e6221c6752520128504e71892ea9691256
newtoncutrim <newtoncutrim777@gmail.com>
2023-12-18 11:08:13 -0300

    metodo update

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,6 +1,13 @@
 import House from "../models/House";
-
+import User from "../models/User";
 class HouseController {
+    
+    async index(req, res){
+        const {status} = req.query;
+        const houses = await House.find({status: status});
+
+        return res.json(houses);
+    }
     async store(req, res){
         console.log(req.headers);
         console.log(req.body);
@@ -9,7 +16,6 @@ class HouseController {
         const {description, price, status, location} = req.body;
         const {filename} = req.file
         const {user_id} = req.body;
-        console.log(filename)
 
         const house = await House.create({
             user: user_id,
@@ -21,6 +27,37 @@ class HouseController {
 
         });
         return res.json(house);
+    }
+
+    async update(req, res) {
+        const {filename} = req.file
+        const {id_house} = req.params;
+        const {description, price, status, location} = req.body;
+        const {user_id} = req.headers;
+
+ 
+        const user = await User.findById(user_id);
+        const house = await House.findById(id_house);
+        console.log(String(user._id))
+        console.log(String(house.user))
+
+        if(String(user._id) !== String(house.user)){
+            console.log("Usuário não autorizado");
+            return res.json({error: "Não autorizado"});
+        }
+
+        const houses = await House.updateOne({_id: id_house}, {
+            user: user_id,
+            thumbnail: filename,
+            description: description,
+            price: price,
+            location: location,
+            status: status
+        });
+
+        return res.send(house);
+        
+
     }
 }
 
--- src/router.js
+++ src/router.js
@@ -10,4 +10,7 @@ const upload = multer(configUpload)
 routes.post('/sessions', SessionController.store);
 
 routes.post('/houses', upload.single('thumbnail') ,HouseController.store);
+routes.get('/houses', HouseController.index);
+routes.put('/houses/:id_house', upload.single('thumbnail'), HouseController.update);
+
 export default routes;
